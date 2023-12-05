58972b9bb208e3226c580a332640ffddc8c3fb11
Paulo Vinícius <paulov@pop-os.localdomain>
2023-12-04 22:38:34 -0300

    Adds series of new functions

--- lua/custom/plugins/colorscheme/everforest.lua
+++ lua/custom/plugins/colorscheme/everforest.lua
@@ -8,14 +8,11 @@ return {
         require("everforest").setup({
             ---Controls the "hardness" of the background. Options are "soft", "medium" or "hard".
             ---Default is "medium".
-            background = "medium",
+            background = "hard",
             ---How much of the background should be transparent. 2 will have more UI
             ---components be transparent (e.g. status line background)
-            transparent_background_level = 2,
-            ---Whether italics should be used for keywords and more.
+            transparent_background_level = 1,
             italics = true,
-            ---Disable italic fonts for comments. Comments are in italics by default, set
-            ---this to `true` to make them _not_ italic!
             disable_italic_comments = false,
             ---By default, the colour of the sign column background is the same as the as normal text
             ---background, but you can use a grey background by setting this to `"grey"`.
@@ -56,15 +53,7 @@ return {
             ---NB: This is only significant for dark backgrounds as the light palettes
             ---have the same colour for both values in the switch.
             float_style = "bright",
-            ---You can override specific highlights to use other groups or a hex colour.
-            ---This function will be called with the highlights and colour palette tables.
-            ---@param highlight_groups Highlights
-            ---@param palette Palette
             on_highlights = function(highlight_groups, palette) end,
-            ---You can override colours in the palette to use different hex colours.
-            ---This function will be called once the base and background colours have
-            ---been mixed on the palette.
-            ---@param palette Palette
             colours_override = function(palette) end,
         })
     end,
