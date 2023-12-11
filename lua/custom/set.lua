6035ad5990d0e98476079f6a53dc1797e2993b78
pvskp <pvincius14@gmail.com>
2023-12-11 10:47:15 -0300

    Removes fidget

--- lua/custom/set.lua
+++ lua/custom/set.lua
@@ -3,7 +3,7 @@ vim.opt.relativenumber = true
 -- vim.opt.cursorline = true
 vim.opt.path = ".,,**"
 vim.opt.wildmenu = true
-
+-- vim.opt.autochdir = true
 
 vim.opt.scrolloff = 99999
 
@@ -13,8 +13,8 @@ vim.cmd([[autocmd FileType * set formatoptions-=ro]])
 
 -- Configure tabs
 vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 
 vim.opt.swapfile = false
 
@@ -33,8 +33,10 @@ vim.o.timeoutlen = 300
 -- Save undo history
 vim.o.undofile = true
 
+-- Set colorscheme
 vim.o.background = "dark" -- or "light" for light mode
 
+-- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
