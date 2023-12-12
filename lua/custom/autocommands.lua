d5433cf58c574c79f9869b73e79f032e622e0183
pvskp <pvincius14@gmail.com>
2023-12-12 11:59:44 -0300

    Autoformat on save

--- lua/custom/autocommands.lua
+++ lua/custom/autocommands.lua
@@ -1,5 +1,21 @@
 -- Term auto-insert
 vim.api.nvim_create_autocmd({ "BufWinEnter", "TermOpen", "WinEnter" }, {
-    pattern = "term://*",
-    command = "startinsert",
+  pattern = "term://*",
+  command = "startinsert",
+})
+
+vim.api.nvim_create_augroup("AutoIndentOnSave", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = "AutoIndentOnSave",
+  pattern = "*",
+  callback = function()
+    -- Salvar a posição atual do cursor
+    local curpos = vim.api.nvim_win_get_cursor(0)
+
+    -- Executar a indentação
+    vim.cmd("normal! gg=G")
+
+    -- Restaurar a posição do cursor
+    vim.api.nvim_win_set_cursor(0, curpos)
+  end
 })
