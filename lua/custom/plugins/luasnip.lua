7545d9c24241459a8ceb4590fe4673c9f2950cd9
pvskp <pvincius14@gmail.com>
2023-12-12 18:17:39 -0300

    Remap luasnip tab

--- lua/custom/plugins/luasnip.lua
+++ lua/custom/plugins/luasnip.lua
@@ -1,6 +1,9 @@
 -- vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
-vim.keymap.set({"i", "s"}, "<Tab>", function() require('luasnip').jump( 1) end, {silent = true})
-vim.keymap.set({"i", "s"}, "<S-Tab>", function() require('luasnip').jump(-1) end, {silent = true})
+-- vim.keymap.set({"i", "s"}, "<Tab>", function() require('luasnip').jump( 1) end, {silent = true})
+-- vim.keymap.set({"i", "s"}, "<S-Tab>", function() require('luasnip').jump(-1) end, {silent = true})
+
+vim.keymap.set({"i", "s"}, "<C-j>", function() require('luasnip').jump( 1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-k>", function() require('luasnip').jump(-1) end, {silent = true})
 return {
   "L3MON4D3/LuaSnip", -- snippet engine,
 }
