ae64322f450c84c29bd7d13d223173595e6c7e17
pvskp <pvincius14@gmail.com>
2023-12-16 13:18:40 -0300

    Gets better at lazy

--- lua/custom/plugins/vim-go.lua
+++ lua/custom/plugins/vim-go.lua
@@ -1,3 +1,23 @@
 return {
-    "fatih/vim-go",
+	"fatih/vim-go",
+	event = "LspAttach",
+	dependencies = {
+		"olexsmir/gopher.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		opts = {
+			commands = {
+				go = "go",
+				gomodifytags = "gomodifytags",
+				gotests = "~/go/bin/gotests", -- also you can set custom command path
+				impl = "impl",
+				iferr = "iferr",
+			},
+		},
+	},
+	config = function()
+		require("gopher")
+	end,
 }
