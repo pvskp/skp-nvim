ec3bc308b942d3a22c2e19e6b75e9e92a19332fd
pvskp <pvincius14@gmail.com>
2023-12-16 17:42:16 -0300

    Adds mini.files

--- lua/custom/plugins/lsp_signature.lua
+++ lua/custom/plugins/lsp_signature.lua
@@ -1,14 +1,10 @@
 return {
-  "ray-x/lsp_signature.nvim",
-  lazy = false,
-  -- event = "VeryLazy",
-  opts = {
-
-    hint_prefix = "🐧 ", -- Panda for parameter, NOTE: for the terminal not support emoji, might crash
-    handler_opts = {
-      border = "double", -- double, rounded, single, shadow, none, or a table of borders
-    },
-    transparency = nil, -- disabled by default, allow floating win transparent value 1~100
-    shadow_blend = 0, -- if you using shadow as border use this set the opacity
-  },
+	"ray-x/lsp_signature.nvim",
+	event = "LspAttach",
+	opts = {
+		hint_prefix = "🐧 ", -- Panda for parameter, NOTE: for the terminal not support emoji, might crash
+		handler_opts = {
+			border = "double", -- double, rounded, single, shadow, none, or a table of borders
+		},
+	},
 }
