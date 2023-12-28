b6f8bd2f12a2bc4fa5dec2afab241a610a29e8f0
pvskp <pvincius14@gmail.com>
2023-12-28 18:10:03 -0300

    Removes some

--- lua/custom/colorscheme.lua
+++ lua/custom/colorscheme.lua
@@ -1,6 +1,6 @@
 -- local colorscheme = "evening"
 -- local colorscheme = "base16-windows-highcontrast"
-local colorscheme = "zenbones"
+local colorscheme = "rose-pine"
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
 	print("colorscheme " .. colorscheme .. " not found.")
--- lua/custom/plugins/lsp/conform.lua
+++ lua/custom/plugins/lsp/conform.lua
@@ -9,5 +9,11 @@ return {
 			javascript = { { "prettierd", "prettier" } },
 			json = { { "prettierd", "prettier" } },
 		},
+
+		format_on_save = {
+			-- I recommend these options. See :help conform.format for details.
+			lsp_fallback = true,
+			timeout_ms = 1000,
+		},
 	},
 }
