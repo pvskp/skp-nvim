f2a07846c77fcc44220b7f0e5ae11dd3cf285dc9
pvskp <pvincius14@gmail.com>
2023-12-30 14:15:15 -0300

    Uses mason to auto setup lsp's

--- lua/custom/plugins/lsp/mason.lua
+++ lua/custom/plugins/lsp/mason.lua
@@ -1,51 +1,124 @@
 return {
-  "williamboman/mason.nvim",
-  lazy = false,
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    local mason_tool_installer = require("mason-tool-installer")
+	"williamboman/mason.nvim",
+	lazy = false,
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
 
-        border = "double",
-      },
-    })
+				border = "double",
+			},
+		})
 
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "lua_ls",
-        "pyright",
-        "gopls",
-        "terraformls",
-      },
-      -- auto-install configured servers (with lspconfig)
-      automatic_installation = true, -- not the same as ensure_installed
-    })
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"lua_ls",
+			},
+			-- auto-install configured servers (with lspconfig)
+			automatic_installation = true, -- not the same as ensure_installed
+		})
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint", -- python linter
-        "eslint_d", -- js linter
-        "goimports",
-      },
-    })
-  end,
+		local keymap = vim.keymap -- for conciseness
+		local opts = { noremap = true, silent = true }
+		local on_attach = function(client, bufnr)
+			opts.buffer = bufnr
+
+			opts.desc = "Uses Navbuddy"
+			keymap.set("n", "<leader>n", ":Navbuddy<CR>", opts)
+
+			opts.desc = "Go to declaration"
+			keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+
+			opts.desc = "Go to references"
+			keymap.set("n", "gr", vim.lsp.buf.references, opts)
+
+			opts.desc = "Show LSP definitions"
+			keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- show lsp definitions
+
+			opts.desc = "See available code actions"
+			keymap.set({ "n", "v" }, "<leader>a", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+
+			opts.desc = "Smart rename"
+			keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts) -- smart rename
+
+			opts.desc = "Show line diagnostics"
+			keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+
+			opts.desc = "Go to previous diagnostic"
+			keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+
+			opts.desc = "Go to next diagnostic"
+			keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+
+			opts.desc = "Show documentation for what is under cursor"
+			keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+
+			opts.desc = "Access code actions"
+			keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts) -- show documentation for what is under cursor
+
+			opts.desc = "Restart LSP"
+			keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+
+			-- vim.highlight.priorities.semantic_tokens = 95
+			-- vim.highlight.priorities.treesitter = 201
+		end
+
+		-- used to enable autocompletion (assign to every lsp server config)
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		capabilities.textDocument.foldingRange = {
+			dynamicRegistration = false,
+			lineFoldingOnly = true,
+		}
+
+		mason_lspconfig.setup_handlers({
+			function(server_name) -- default handler (optional)
+				require("lspconfig")[server_name].setup({
+					capabilities = capabilities,
+					on_attach = on_attach,
+				})
+			end,
+			["lua_ls"] = function()
+				require("lspconfig")["lua_ls"].setup({
+					capabilities = capabilities,
+					on_attach = on_attach,
+					settings = { -- custom settings for lua
+						Lua = {
+							-- make the language server recognize "vim" global
+							diagnostics = {
+								globals = { "vim" },
+							},
+							workspace = {
+								-- make language server aware of runtime files
+								library = {
+									[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+									[vim.fn.stdpath("config") .. "/lua"] = true,
+								},
+							},
+						},
+					},
+				})
+			end,
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"stylua", -- lua formatter
+			},
+		})
+	end,
 }
