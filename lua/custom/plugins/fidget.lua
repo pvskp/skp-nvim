1ba1ed156ea5d28b195c68ceff9d33d52577c2c1
pvskp <pvincius14@gmail.com>
2024-01-05 19:29:58 -0300

    Fix fidget config

--- lua/custom/plugins/fidget.lua
+++ lua/custom/plugins/fidget.lua
@@ -2,4 +2,11 @@ return {
 	"j-hui/fidget.nvim",
 	config = true,
 	lazy = false,
+	opts = {
+		notification = {
+			window = {
+				winblend = 0, -- Background color opacity in the notification window
+			},
+		},
+	},
 }
