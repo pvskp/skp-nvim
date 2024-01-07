9afbfe9423814d2e6d509bcb3e61b533bc02878d
pvskp <pvincius14@gmail.com>
2024-01-07 01:50:54 -0300

    Creates function to copy highlight group

--- lua/custom/globals.lua
+++ lua/custom/globals.lua
@@ -8,3 +8,21 @@ LAZY_PLUGIN_SPEC = {}
 function Spec(item)
 	table.insert(LAZY_PLUGIN_SPEC, { import = item })
 end
+
+function FormatColor(color)
+	if not color then
+		return nil
+	end
+
+	return string.format("#%06x", color)
+end
+
+function CopyHighlight(src, dest)
+	local string_hl = vim.api.nvim_get_hl_by_name(src, true)
+	vim.api.nvim_set_hl(0, dest, {
+		fg = FormatColor(string_hl.foreground),
+		bg = FormatColor(string_hl.background),
+		bold = string_hl.bold,
+		italic = string_hl.italic,
+	})
+end
