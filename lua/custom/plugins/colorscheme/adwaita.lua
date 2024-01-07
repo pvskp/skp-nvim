165dceaef7fb0a5baf81cd7f3b2592544434a084
pvskp <pvincius14@gmail.com>
2024-01-07 04:14:10 -0300

    Creates my own snippets

--- lua/custom/plugins/colorscheme/adwaita.lua
+++ lua/custom/plugins/colorscheme/adwaita.lua
@@ -5,7 +5,7 @@ return {
 
 	-- configure and set on startup
 	config = function()
-		vim.g.adwaita_darker = true -- for darker version
+		vim.g.adwaita_darker = false -- for darker version
 		vim.g.adwaita_disable_cursorline = true -- to disable cursorline
 		vim.g.adwaita_transparent = true -- makes the background transparent
 		vim.cmd("colorscheme adwaita")
