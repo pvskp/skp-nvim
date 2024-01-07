ae212566adbdd7a72cab6864301a7405f755d0c2
pvskp <pvincius14@gmail.com>
2024-01-07 04:03:20 -0300

    Adds adwaita

--- lua/custom/plugins/lsp/treesiter.lua
+++ lua/custom/plugins/lsp/treesiter.lua
@@ -10,7 +10,7 @@ return {
 
 			highlight = {
 				enable = true,
-				disable = { "markdown", "vimdoc" },
+				disable = { "markdown", "vimdoc", "help" },
 				additional_vim_regex_highlighting = false,
 				indent = {
 					enable = false,
