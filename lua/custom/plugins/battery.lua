0dce5362db9572fb7b45cdf65a84ed04841f463f
pvskp <pvincius14@gmail.com>
2024-01-07 02:03:33 -0300

    Changes gitsigns color

--- lua/custom/plugins/battery.lua
+++ lua/custom/plugins/battery.lua
@@ -3,7 +3,7 @@ return {
 	-- dir = "~/Documents/repos/battery.nvim/",
 	config = function()
 		require("battery").setup({
-			update_rate_seconds = 10, -- Number of seconds between checking battery status
+			update_rate_seconds = 30, -- Number of seconds between checking battery status
 			show_status_when_no_battery = true, -- Don't show any icon or text when no battery found (desktop for example)
 			show_plugged_icon = true, -- If true show a cable icon alongside the battery icon when plugged in
 			show_unplugged_icon = true, -- When true show a diconnected cable icon when not plugged in
