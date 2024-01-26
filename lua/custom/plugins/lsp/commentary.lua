ff47891a90fcddf3d7d38005043d363fb7be8fa4
pvskp <pvincius14@gmail.com>
2024-01-26 02:18:09 -0300

    Little improvements

--- lua/custom/plugins/lsp/commentary.lua
+++ lua/custom/plugins/lsp/commentary.lua
@@ -1,4 +1,8 @@
 return {
-  "tpope/vim-commentary",
-  lazy = false,
+	"tpope/vim-commentary",
+	lazy = false,
+	keys = {
+		{ "<leader>c", "<Plug>CommentaryLine", { silent = true }, mode = "n" },
+		{ "<leader>c", "<Plug>Commentary", { silent = true }, mode = "v" },
+	},
 }
