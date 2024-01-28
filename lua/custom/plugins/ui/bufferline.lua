360b579ca9a9ea15472a1baf4def0e459299c9eb
pvskp <pvincius14@gmail.com>
2024-01-27 22:20:00 -0300

    organize bufferline cfg

--- lua/custom/plugins/ui/bufferline.lua
+++ lua/custom/plugins/ui/bufferline.lua
@@ -1,48 +1,45 @@
 return {
 	"akinsho/bufferline.nvim",
 	lazy = false,
-	opts = {
-		options = {
-			mode = "buffers", -- set to "tabs" to only show tabpages instead
-			-- themable = true | false, -- allows highlight groups to be overriden i.e. sets highlights as default
-			-- numbers = "none" | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-			numbers = "none",
-			close_command = "bdelete! %d", -- can be a string | function, | false see "Mouse actions"
-			right_mouse_command = "bdelete! %d", -- can be a string | function | false, see "Mouse actions"
-			left_mouse_command = "buffer %d", -- can be a string | function, | false see "Mouse actions"
-			middle_mouse_command = nil, -- can be a string | function, | false see "Mouse actions"
-			indicator = {
-				-- icon = "▎", -- this should be omitted if indicator style is not 'icon'
-				style = "none",
+	config = function()
+		local bufferline = require("bufferline")
+		bufferline.setup({
+			options = {
+				mode = "buffers", -- set to "tabs" to only show tabpages instead
+				numbers = "none", -- "none" | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+				style_preset = {
+					bufferline.style_preset.no_italic,
+				},
+				close_command = "bdelete! %d", -- can be a string | function, | false see "Mouse actions"
+				right_mouse_command = "bdelete! %d", -- can be a string | function | false, see "Mouse actions"
+				left_mouse_command = "buffer %d", -- can be a string | function, | false see "Mouse actions"
+				middle_mouse_command = nil, -- can be a string | function, | false see "Mouse actions"
+				indicator = {
+					-- icon = "▎", -- this should be omitted if indicator style is not 'icon'
+					style = "none",
+				},
+				buffer_close_icon = "󰅖",
+				modified_icon = "●",
+				close_icon = "",
+				left_trunc_marker = "",
+				right_trunc_marker = "",
+				max_name_length = 18,
+				max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+				truncate_names = true, -- whether or not tab names should be truncated
+				tab_size = 16,
+				diagnostics = "none",
+				color_icons = true, -- whether or not to add the filetype icon highlights
+				show_buffer_icons = true, -- disable filetype icons for buffers
+				show_buffer_close_icons = false,
+				show_close_icon = true,
+				show_tab_indicators = true,
+				show_duplicate_prefix = true, -- whether to show duplicate buffer prefix
+				persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+				move_wraps_at_ends = false, -- whether or not the move command "wraps" at the first or last position
+				always_show_bufferline = false,
 			},
-			buffer_close_icon = "󰅖",
-			modified_icon = "●",
-			close_icon = "",
-			left_trunc_marker = "",
-			right_trunc_marker = "",
-			max_name_length = 18,
-			max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
-			truncate_names = true, -- whether or not tab names should be truncated
-			tab_size = 18,
-			diagnostics = "nvim_lsp",
-			diagnostics_update_in_insert = false,
-			color_icons = true, -- whether or not to add the filetype icon highlights
-			show_buffer_icons = true, -- disable filetype icons for buffers
-			show_buffer_close_icons = true,
-			show_close_icon = true,
-			show_tab_indicators = true,
-			show_duplicate_prefix = true, -- whether to show duplicate buffer prefix
-			persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-			move_wraps_at_ends = false, -- whether or not the move command "wraps" at the first or last position
-			-- can also be a table containing 2 custom separators
-			-- [focused and unfocused]. eg: { '|', '|' }
-			-- separator_style = "slant" | "slope" | "thick" | "thin" | { "any", "any" },
-			-- enforce_regular_tabs = false | true,
-			always_show_bufferline = false,
-		},
-	},
-	config = function(_, opts)
-		require("bufferline").setup(opts)
+		})
+
 		vim.keymap.set("n", "<leader><Tab>", "<cmd>BufferLinePick<CR>", { silent = true })
 	end,
 }
