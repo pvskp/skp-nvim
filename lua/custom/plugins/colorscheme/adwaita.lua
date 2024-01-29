d15be1b95dd25be491b932b8f6ce466fa339ed7f
pvskp <pvincius14@gmail.com>
2024-01-29 15:23:17 -0300

    Adds keys to oil.lua

--- lua/custom/plugins/colorscheme/adwaita.lua
+++ lua/custom/plugins/colorscheme/adwaita.lua
@@ -7,7 +7,7 @@ return {
 	config = function()
 		vim.g.adwaita_darker = false -- for darker version
 		vim.g.adwaita_disable_cursorline = true -- to disable cursorline
-		vim.g.adwaita_transparent = true -- makes the background transparent
+		vim.g.adwaita_transparent = false -- makes the background transparent
 		vim.cmd("colorscheme adwaita")
 	end,
 }
