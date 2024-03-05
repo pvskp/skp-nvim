0493b807077ea90daca4606b3bdb788f9f6b5f24
pvskp <pvincius14@gmail.com>
2024-03-05 10:59:44 -0300

    Adds obsidian.nvim

--- lua/custom/plugins/languages/python.lua
+++ lua/custom/plugins/languages/python.lua
@@ -1,6 +1,6 @@
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-	pattern = "*.py",
-	callback = function()
-		vim.fn.system("autoimport " .. vim.fn.expand("%"))
-	end,
-})
+-- vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+-- 	pattern = "*.py",
+-- 	callback = function()
+-- 		vim.fn.system("autoimport " .. vim.fn.expand("%"))
+-- 	end,
+-- })
