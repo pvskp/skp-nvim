adc5bb9ac0f4cb50d34cb428ca218c31a82175dc
pvskp <pvincius14@gmail.com>
2024-03-22 08:25:43 -0300

    a

--- lua/custom/plugins/ui/lualine.lua
+++ lua/custom/plugins/ui/lualine.lua
@@ -91,5 +91,8 @@ return {
 			inactive_winbar = {},
 			extensions = {},
 		})
+
+		-- vim.cmd([[hi Container guifg=#BADA55 guibg=Black]])
+		-- vim.cmd([[set statusline+=%#Container#%{g:currentContainer}]])
 	end,
 }
