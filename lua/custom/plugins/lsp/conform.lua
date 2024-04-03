3d2eb1a9309448fb8da7acced387e7a426640c2b
pvskp <pvincius14@gmail.com>
2024-04-03 09:07:22 -0300

    MOdifies conform

--- lua/custom/plugins/lsp/conform.lua
+++ lua/custom/plugins/lsp/conform.lua
@@ -13,10 +13,24 @@ return {
 			cpp = { "clang-format" },
 		},
 
-		format_on_save = {
-			-- I recommend these options. See :help conform.format for details.
-			lsp_fallback = true,
-			timeout_ms = 1000,
-		},
+		-- format_on_save = {
+		-- 	-- I recommend these options. See :help conform.format for details.
+		-- 	lsp_fallback = true,
+		-- 	timeout_ms = 1000,
+		-- },
 	},
+	config = function(_, opts)
+		require("conform").setup(opts)
+
+		-- Use this instead format_on_save so I can exclude specific files
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(ctx)
+				-- if vim.fs.basename(ctx.filename) ~= "rc.lua"
+				if vim.fn.expand("%:t") ~= "rc.lua" then
+					require("conform").format({ bufnr = ctx.buf })
+				end
+			end,
+		})
+	end,
 }
