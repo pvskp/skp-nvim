6f7ed0c23093dd45611ef5fbf815b2eadc40bb4f
pvskp <pvincius14@gmail.com>
2024-04-05 11:04:13 -0300

    a

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -1,13 +1,14 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.opt.termguicolors = true
-
 return {
 	"nvim-tree/nvim-tree.lua",
-	lazy = false,
+	event = {},
 	keys = {
 		{ "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle File Explorer" },
 	},
+	init = function()
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+		-- vim.opt.termguicolors = true
+	end,
 	opts = {
 		view = {
 			side = "left",
