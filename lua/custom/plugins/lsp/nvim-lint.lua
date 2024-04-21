a91e6a875a4b1d3462de54a1f82f62f09c4d933f
pvskp <pvincius14@gmail.com>
2024-04-21 09:46:57 -0300

    linter

--- lua/custom/plugins/lsp/nvim-lint.lua
+++ lua/custom/plugins/lsp/nvim-lint.lua
@@ -1,34 +1,34 @@
 return {
-	"mfussenegger/nvim-lint",
-	event = {
-		"BufReadPre",
-		"BufNewFile",
-	},
-	config = function()
-		local lint = require("lint")
+  'mfussenegger/nvim-lint',
+  event = {
+    'BufReadPre',
+    'BufNewFile',
+  },
+  config = function()
+    local lint = require 'lint'
 
-		lint.linters_by_ft = {
-			javascript = { "eslint_d" },
-			typescript = { "eslint_d" },
-			json = { "jsonlint" },
-			python = { "ruff", "flake8" },
-			go = { "golangcilint" },
-			terraform = { "tflint", "tfsec" },
-			-- cpp = { "cpplint" },
-			-- yaml = { "yamllint" },
-			-- lua = { "luacheck" },
-			-- python = { "flake8" },
-		}
+    lint.linters_by_ft = {
+      javascript = { 'eslint_d' },
+      typescript = { 'eslint_d' },
+      json = { 'jsonlint' },
+      python = { 'ruff' },
+      go = { 'golangcilint' },
+      terraform = { 'tflint', 'tfsec' },
+      -- cpp = { "cpplint" },
+      -- yaml = { "yamllint" },
+      -- lua = { "luacheck" },
+      -- python = { "flake8" },
+    }
 
-		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-			group = lint_augroup,
-			callback = function()
-				lint.try_lint()
+    local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+      group = lint_augroup,
+      callback = function()
+        lint.try_lint()
 
-				-- local linters = lint.get_running()
-				-- P(linters)
-			end,
-		})
-	end,
+        -- local linters = lint.get_running()
+        -- P(linters)
+      end,
+    })
+  end,
 }
