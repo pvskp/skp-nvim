9a35ddb1a5fe6ce667e0e91af686ff797a3f8055
pvskp <pvincius14@gmail.com>
2024-04-21 15:47:55 -0300

    Sort init.lua and removes mason.lua (not the plugin)

--- init.lua
+++ init.lua
@@ -33,17 +33,16 @@ spec(poff, 'colorscheme.adwaita')
 --<< LSP related >>--
 
 spec(pon, 'lsp.treesiter') -- love and hate relationship with this one
-spec(poff, 'lsp.rainbow-delimiters') -- show matching delimiters
+spec(pon, 'lsp.nvim-lint') -- linting
 spec(pon, 'lsp.neodev') -- offers completion to neovim functions
 spec(pon, 'lsp.navbuddy') -- allow to navigate between symbols in a easir way
-spec(poff, 'lsp.mason') -- download lsp servers, fomratters, etc
 spec(pon, 'lsp.luasnip') -- snippets
 spec(pon, 'lsp.lspconfig')
 spec(pon, 'lsp.conform') -- formatter
 spec(pon, 'lsp.commentary')
 spec(pon, 'lsp.cmp') -- completion
+spec(poff, 'lsp.rainbow-delimiters') -- show matching delimiters
 spec(poff, 'lsp.outline')
-spec(pon, 'lsp.nvim-lint') -- linting
 spec(poff, 'lsp.lsp-saga')
 
 --<< Language specific >>--
@@ -60,26 +59,25 @@ spec(poff, 'mini.files') -- minitree (best tree plugin)
 --<< Git integrations >>--
 
 spec(pon, 'git.lazygit') -- lazygit inside neovim
-spec(poff, 'git.gitsigns') -- show git signs on columnsign
 spec(pon, 'git.fugitive') -- git integration
-spec(poff, 'git.diffview')
 spec(poff, 'git.neogit')
+spec(poff, 'git.gitsigns') -- show git signs on columnsign
+spec(poff, 'git.diffview')
 spec(poff, 'git.blame')
 
 --<< General >>--
 
 spec(pon, 'undotree') -- show undo tree
-spec(pon, 'neo-zoom') -- toggle zoom mode with a popup window
 spec(pon, 'trouble') -- cool quickfix list for LSP
 spec(pon, 'toggle-checkbox')
 spec(pon, 'tmux') -- tmux integration
 spec(pon, 'telescope') -- fuzzy finder
 spec(pon, 'surround') -- surround text with quotes, brackets, etc
-spec(poff, 'remote-containers')
 spec(pon, 'oil')
 spec(pon, 'obsidian')
 spec(pon, 'nvim-tree')
 spec(pon, 'neogen')
+spec(pon, 'neo-zoom') -- toggle zoom mode with a popup window
 spec(pon, 'markdown-preview')
 spec(pon, 'kitty') -- kitty integration
 spec(pon, 'hop') -- jump to any word in the screen
@@ -87,7 +85,6 @@ spec(pon, 'focus') -- auto-resize windows when switching buffers
 spec(pon, 'eyeliner') -- quick jump with f, F, t, T
 spec(pon, 'discord') -- rich presence
 spec(pon, 'btterm') -- this one is mine!!!!
-spec(poff, 'autopairs')
 spec(pon, 'auto-session')
 spec(poff, 'wilder')
 spec(poff, 'which-key')
@@ -95,6 +92,7 @@ spec(poff, 'vim-be-good')
 spec(poff, 'ufo') -- deal with folding in a better way
 spec(poff, 'tetris')
 spec(poff, 'spider')
+spec(poff, 'remote-containers')
 spec(poff, 'remote-containers')
 spec(poff, 'neoscroll')
 spec(poff, 'neo-tree')
@@ -109,6 +107,7 @@ spec(poff, 'flybuf')
 spec(poff, 'fakeclip')
 spec(poff, 'color-picker')
 spec(poff, 'cmdbuf')
+spec(poff, 'autopairs')
 
 --<< AI >>--
 
@@ -119,22 +118,22 @@ spec(poff, 'ai.copilot')
 
 --<< UI >>--
 
+spec(pon, 'ui.nvim-bqf')
 spec(pon, 'ui.lualine') -- statusline
-spec(pon, 'ui.devicons')
 spec(pon, 'ui.indentlines') -- show indent lines
+spec(pon, 'ui.illuminate')
 spec(pon, 'ui.fidget') -- cool LSP notification
 spec(pon, 'ui.dressing') -- offers better ui for some neovim commands
+spec(pon, 'ui.devicons')
 spec(pon, 'ui.colorizer') -- color preview
 spec(pon, 'ui.barbecue') -- shows the current symbol and its parents
-spec(pon, 'ui.nvim-bqf')
-spec(poff, 'ui.transparent')
-spec(poff, 'ui.bufferline')
 spec(poff, 'ui.virt-column')
 spec(poff, 'ui.twilight')
+spec(poff, 'ui.transparent')
 spec(poff, 'ui.sunglasses')
 spec(poff, 'ui.netrw')
-spec(pon, 'ui.illuminate')
 spec(poff, 'ui.headlines')
+spec(poff, 'ui.bufferline')
 spec(poff, 'ui.alpha') -- start dashboard
 
 --<< Lualine integrations >>--
