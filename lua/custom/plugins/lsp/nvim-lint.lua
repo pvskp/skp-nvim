a9c4eccbd7fa72b9f47cce98727463d56028b3c5
pvskp <pvincius14@gmail.com>
2024-04-30 10:32:22 -0300

    Uses ruff as python linter

--- lua/custom/plugins/lsp/nvim-lint.lua
+++ lua/custom/plugins/lsp/nvim-lint.lua
@@ -14,7 +14,7 @@ return {
       lua = { 'luacheck' },
       go = { 'golangcilint' },
       terraform = { 'tflint', 'tfsec' },
-      -- python = { 'ruff' },
+      python = { 'ruff' },
       -- cpp = { "cpplint" },
       -- yaml = { "yamllint" },
       -- python = { "flake8" },
