49df56daedb3d9fa342cb6327803970513c81e51
pvskp <pvincius14@gmail.com>
2024-05-08 08:32:06 -0300

    Do not format yaml files

--- lua/custom/plugins/lsp/conform.lua
+++ lua/custom/plugins/lsp/conform.lua
@@ -9,7 +9,7 @@ return {
       python = { 'ruff_format' },
       javascript = { { 'prettierd', 'prettier' } },
       json = { { 'prettierd', 'prettier' } },
-      yaml = { 'prettierd' },
+      -- yaml = { 'prettierd' },
       cpp = { 'clang-format' },
       terraform = { 'terraform_fmt' },
       zsh = { 'beautysh' },
