8d7dd7df7a448363e778d57689e709558684c03e
pvskp <pvincius14@gmail.com>
2024-05-14 08:55:58 -0300

    Formats kanagawa

--- lua/custom/plugins/colorscheme/kanagawa.lua
+++ lua/custom/plugins/colorscheme/kanagawa.lua
@@ -1,26 +1,27 @@
 return {
-	"rebelot/kanagawa.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = {
-		compile = false, -- enable compiling the colorscheme
-		undercurl = true, -- enable undercurls
-		commentStyle = { italic = true },
-		functionStyle = {},
-		keywordStyle = { bold = true, italic = false },
-		statementStyle = { bold = true },
-		typeStyle = {},
-		transparent = false, -- do not set background color
-		dimInactive = true, -- dim inactive window `:h hl-NormalNC`
-		terminalColors = true, -- define vim.g.terminal_color_{0,17}
-		theme = "wave", -- Load "wave" theme when 'background' option is not set
-		background = { -- map the value of 'background' option to a theme
-			dark = "wave", -- try "dragon" !
-			light = "lotus",
-		},
-	},
-	config = function(_, opts)
-		require("kanagawa").setup(opts)
-		vim.cmd([[colorscheme kanagawa]])
-	end,
+  'rebelot/kanagawa.nvim',
+  lazy = false,
+  priority = 1000,
+  opts = {
+    compile = false, -- enable compiling the colorscheme
+    undercurl = true, -- enable undercurls
+    commentStyle = { italic = true },
+    functionStyle = {},
+    keywordStyle = { bold = true, italic = false },
+    statementStyle = { bold = true },
+    typeStyle = {},
+    transparent = false, -- do not set background color
+    dimInactive = true, -- dim inactive window `:h hl-NormalNC`
+    terminalColors = true, -- define vim.g.terminal_color_{0,17}
+    theme = 'wave', -- Load "wave" theme when 'background' option is not set
+    background = { -- map the value of 'background' option to a theme
+      -- dark = "wave", -- try "dragon" !
+      dark = 'dragon', -- try "dragon" !
+      light = 'lotus',
+    },
+  },
+  config = function(_, opts)
+    require('kanagawa').setup(opts)
+    vim.cmd [[colorscheme kanagawa]]
+  end,
 }
