8c2e432318e4d7e23cc634d8a7d14c20f08701ae
pvskp <pvincius14@gmail.com>
2024-05-14 01:29:20 -0300

    Add 'fonts' source for cmp on toml files

--- lua/custom/plugins/lsp/cmp.lua
+++ lua/custom/plugins/lsp/cmp.lua
@@ -5,7 +5,7 @@ return {
 
     --< nvim-cmp plugins >--
     'hrsh7th/cmp-buffer', -- source for text in buffer
-    'hrsh7th/cmp-path', -- source for file system paths
+    'hrsh7th/cmp-path',   -- source for file system paths
     'hrsh7th/cmp-cmdline',
     'hrsh7th/cmp-nvim-lsp-signature-help',
     'amarakon/nvim-cmp-fonts', -- source for font completion
@@ -56,7 +56,7 @@ return {
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4), -- show completion suggestions
         ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(), -- close completion window
+        ['<C-e>'] = cmp.mapping.abort(),        -- close completion window
         -- ["<CR>"] = cmp.mapping.confirm({ select = false }),
         ['<CR>'] = cmp.mapping.confirm { select = true },
         ['<Tab>'] = cmp.mapping.confirm { select = true },
@@ -69,8 +69,8 @@ return {
         { name = 'codeium' },
         { name = 'copilot' },
         { name = 'luasnip' }, -- snippets
-        { name = 'path' }, -- file system paths
-        { name = 'buffer' }, -- text within current buffer
+        { name = 'path' },    -- file system paths
+        { name = 'buffer' },  -- text within current buffer
       },
 
       -- configure lspkind for vs-code like pictograms in completion menu
@@ -101,7 +101,7 @@ return {
       vim.cmd 'highlight! BorderBG guibg=NONE'
     end
 
-    cmp.setup.filetype({ 'conf', 'config', 'kitty' }, {
+    cmp.setup.filetype({ 'conf', 'config', 'kitty', 'toml' }, {
       sources = {
         { name = 'fonts', option = { space_filter = '-' } },
         { name = 'path' },
