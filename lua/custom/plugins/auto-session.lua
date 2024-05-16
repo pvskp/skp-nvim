6d4d6c0d5a976e8f7fa270d2d0c3d66f3ddf652d
pvskp <pvincius14@gmail.com>
2024-05-16 09:31:02 -0300

    Autosession does not depend on telescope

--- lua/custom/plugins/auto-session.lua
+++ lua/custom/plugins/auto-session.lua
@@ -1,11 +1,20 @@
 return {
-	"rmagatti/auto-session",
-	lazy = false,
-	init = function()
-		vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-	end,
-	opts = {
-		log_level = "error",
-		auto_session_suppress_dirs = {},
-	},
+  'rmagatti/auto-session',
+  lazy = false,
+  init = function()
+    vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
+  end,
+  opts = {
+    log_level = 'error',
+    auto_session_suppress_dirs = {},
+    -- ⚠️ This will only work if Telescope.nvim is installed
+    -- The following are already the default values, no need to provide them if these are already the settings you want.
+    session_lens = {
+      -- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
+      buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
+      load_on_setup = false,
+      theme_conf = { border = true },
+      previewer = false,
+    },
+  },
 }
