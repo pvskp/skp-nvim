95cad9e30fdae9c5022824c9fcb65b7c95b6ee6a
pvskp <pvincius14@gmail.com>
2024-05-16 10:25:48 -0300

    Remove useless comment

--- lua/custom/plugins/git/gitsigns.lua
+++ lua/custom/plugins/git/gitsigns.lua
@@ -55,8 +55,6 @@ return {
     },
   },
   config = function(_, opts)
-    -- vim.opt.signcolumn = 'yes'
-
     require('gitsigns').setup(opts)
     vim.api.nvim_set_hl(0, 'GitSignsCurrentLineBlame', {
       fg = '#909090',
