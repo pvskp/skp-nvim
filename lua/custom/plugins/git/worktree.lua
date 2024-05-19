a14f8b34550f859d116efe193686f8c09d8ec498
pvskp <pvincius14@gmail.com>
2024-05-19 02:06:08 -0300

    Add separeted file do git-worktree

---
+++ lua/custom/plugins/git/worktree.lua
@@ -0,0 +1,13 @@
+return {
+  'ThePrimeagen/git-worktree.nvim',
+  event = 'VeryLazy',
+  depends = {
+    'nvim-telescope/telescope.nvim',
+  },
+  config = function()
+    require('telescope').load_extension 'git_worktree'
+    local ext = require('telescope').extensions
+    vim.keymap.set('n', '<leader>w', ext.git_worktree.git_worktrees, {})
+    vim.keymap.set('n', '<leader>,', ext.git_worktree.create_git_worktree, {})
+  end,
+}
