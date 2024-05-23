1003d813cecf9bbc972975ac5028051edb3cdd96
pvskp <pvincius14@gmail.com>
2024-05-23 09:08:37 -0300

    custom highlights

--- lua/custom/highlight.lua
+++ lua/custom/highlight.lua
@@ -1,22 +1,6 @@
 vim.cmd 'highlight! NormalFloat guibg=NONE'
 vim.cmd 'highlight! FloatBorder guibg=NONE'
 
--- Adiciona o padrão de realce para "on"
-vim.api.nvim_exec(
-  [[
-hi onPattern guifg=green
-call matchadd('onPattern', '\<pon\>')
-]],
-  false
-)
-
--- Adiciona o padrão de realce para "off"
-vim.api.nvim_exec(
-  [[
-hi offPattern guifg=red
-call matchadd('offPattern', '\<poff\>')
-]],
-  false
-)
-
--- vim.cmd([[highlight Whitespace guifg=Blue ctermfg=blue]])
+vim.cmd 'highlight! link MiniFilesBorder TelescopeResultsBorder'
+vim.cmd 'highlight! link LazyGitBorder TelescopeResultsBorder'
+-- vim.cmd 'highlight! link LazyGitFloat TelescopeResultsBorder'
