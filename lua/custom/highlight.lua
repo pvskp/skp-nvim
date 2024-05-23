3419bed5c669caeee9d9a4cd9c5b108320ed7cc0
pvskp <pvincius14@gmail.com>
2024-05-23 09:11:38 -0300

    transparent telescope

--- lua/custom/highlight.lua
+++ lua/custom/highlight.lua
@@ -3,4 +3,8 @@ vim.cmd 'highlight! FloatBorder guibg=NONE'
 
 vim.cmd 'highlight! link MiniFilesBorder TelescopeResultsBorder'
 vim.cmd 'highlight! link LazyGitBorder TelescopeResultsBorder'
+
+vim.cmd 'highlight TelescopeNormal guibg=NONE'
+vim.cmd 'highlight TelescopeBorder guibg=NONE'
+vim.cmd 'highlight TelescopeTitle guibg=NONE'
 -- vim.cmd 'highlight! link LazyGitFloat TelescopeResultsBorder'
