2e3e95afae067c4889fc24653593dc31370b9182
pvskp <pvincius14@gmail.com>
2024-05-27 17:32:49 -0300

    signcolumn

--- lua/custom/plugins/lsp/lspconfig.lua
+++ lua/custom/plugins/lsp/lspconfig.lua
@@ -24,7 +24,11 @@ return {
 
     for type, icon in pairs(signs) do
       local hl = 'DiagnosticSign' .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
+      vim.fn.sign_define(hl, {
+        text = icon,
+        texthl = hl,
+        numhl = '',
+      })
     end
 
     local mason = require 'mason'
