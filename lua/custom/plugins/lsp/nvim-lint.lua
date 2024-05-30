f45fe81293a169ff338591e6960c65419cd7eeec
pvskp <pvincius14@gmail.com>
2024-05-30 14:57:13 -0300

    checkpoint

--- lua/custom/plugins/lsp/nvim-lint.lua
+++ lua/custom/plugins/lsp/nvim-lint.lua
@@ -11,7 +11,7 @@ return {
       javascript = { 'eslint_d' },
       typescript = { 'eslint_d' },
       json = { 'jsonlint' },
-      lua = { 'luacheck' },
+      -- lua = { 'luacheck' },
       go = { 'golangcilint' },
       terraform = { 'tflint', 'tfsec' },
       python = { 'ruff' },
