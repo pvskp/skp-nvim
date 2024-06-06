c74d9d15ba3705964ac5d87a9500fa564340046b
pvskp <pvincius14@gmail.com>
2024-06-06 20:21:07 -0300

    Sets diagnostics col to width 1

--- lua/custom/plugins/statuscol.lua
+++ lua/custom/plugins/statuscol.lua
@@ -9,7 +9,7 @@ return {
       relculright = true,
       segments = {
         {
-          sign = { namespace = { "diagnostic" }, maxwidth = 1, colwidth = 2, auto = false },
+          sign = { namespace = { "diagnostic" }, maxwidth = 1, colwidth = 1, auto = false },
           click = "v:lua.ScSa",
         },
         { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa", },
