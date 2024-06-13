ea71504e9259a2001859d43db5674de1b496abdf
pvskp <pvincius14@gmail.com>
2024-06-13 08:54:25 -0300

    Bring hop spec back

---
+++ lua/custom/plugins/hop.lua
@@ -0,0 +1,14 @@
+return {
+  'smoka7/hop.nvim',
+  -- lazy = false,
+  version = '*',
+  keys = {
+    { 's', '<cmd>HopChar1<CR>', { silent = true } },
+  },
+  opts = {},
+
+  -- init = function()
+  -- 	require("hop")
+  -- 	vim.keymap.set("", "s", ":HopChar1<CR>", { silent = true })
+  -- end,
+}
