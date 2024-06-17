a54ae8f597fe48777e4a39a0da2a50ea164d556e
pvskp <pvincius14@gmail.com>
2024-06-17 19:36:32 -0300

    terminal mappings

---
+++ lua/custom/terminal.lua
@@ -0,0 +1,38 @@
+local set = vim.opt_local
+
+-- Set local settings for terminal buffers
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-term-open", {}),
+  callback = function()
+    set.number = false
+    set.relativenumber = false
+    set.scrolloff = 0
+
+    vim.bo.filetype = "terminal"
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    if vim.bo.filetype == "terminal" then
+      vim.cmd('startinsert')
+    end
+  end
+})
+
+-- Open a terminal at the bottom of the screen with a fixed height.
+vim.keymap.set("n", ",st", function()
+  vim.cmd.new()
+  vim.cmd.wincmd "J"
+  vim.api.nvim_win_set_height(0, 12)
+  vim.wo.winfixheight = true
+  vim.cmd.term()
+  vim.cmd('startinsert')
+end)
+
+-- Terminal mode mappings
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+vim.keymap.set('t', '<M-j>', '<C-\\><C-n><C-w>j', { silent = true })
+vim.keymap.set('t', '<M-k>', '<C-\\><C-n><C-w>k', { silent = true })
+vim.keymap.set('t', '<M-l>', '<C-\\><C-n><C-w>l', { silent = true })
+vim.keymap.set('t', '<M-h>', '<C-\\><C-n><C-w>h', { silent = true })
