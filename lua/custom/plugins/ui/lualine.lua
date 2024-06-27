6420ea1a541188f1f2bca2928e4a496677d6f68e
pvskp <pvincius14@gmail.com>
2024-06-26 23:58:47 -0300

    new

--- lua/custom/plugins/ui/lualine.lua
+++ lua/custom/plugins/ui/lualine.lua
@@ -5,13 +5,8 @@ return {
     vim.opt.showmode = false
   end,
 
-  -- lazy = false,
   config = function()
-    -- local opts = require 'custom.plugins.ui.lualine.themes.organic'
-    -- local opts = require 'custom.plugins.ui.lualine.themes.supa'
     local opts = require 'custom.plugins.ui.lualine.themes.supax'
-    -- local opts = require 'lualine.themes.supa'
-    -- local opts = {}
     require('lualine').setup(opts)
   end,
 }
