79429499bad008a4262c9acd4059b99c73786a85
pvskp <pvincius14@gmail.com>
2024-07-03 20:22:28 -0300

    config lsp_signature

--- lua/custom/plugins/lsp/lsp_signature.lua
+++ lua/custom/plugins/lsp/lsp_signature.lua
@@ -1,5 +1,16 @@
 return {
   'ray-x/lsp_signature.nvim',
   event = 'LspAttach',
-  config = true,
+  opts = {
+    hint_prefix = "🐼 ", -- Panda for parameter
+    always_trigger = true,
+    toggle_key = "<C-x>",
+    bind = true, -- This is mandatory, otherwise border config won't get registered.
+    handler_opts = {
+      border = Borders.simple
+    }
+  },
+  config = function(_, opts)
+    require("lsp_signature").setup(opts)
+  end
 }
