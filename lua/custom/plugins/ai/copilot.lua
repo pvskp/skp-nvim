dfbbf93ecf68dd480b8545a50db151cb0af6b86e
pvskp <pvincius14@gmail.com>
2024-07-06 16:53:22 -0300

    Re-add the copilot plugin and the default lualine colors

---
+++ lua/custom/plugins/ai/copilot.lua
@@ -0,0 +1,30 @@
+return {
+  "zbirenbaum/copilot.lua",
+  name = "copilot",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  dependencies = {
+    {
+      "zbirenbaum/copilot-cmp",
+      config = function()
+        require("copilot_cmp").setup()
+      end
+    }
+  },
+  opts = {
+    suggestion = { enabled = false },
+    panel = { enabled = false },
+    filetypes = {
+      yaml = true,
+      markdown = true,
+      help = true,
+      gitcommit = true,
+      gitrebase = true,
+      hgcommit = true,
+      svn = true,
+      cvs = true,
+      ["."] = true,
+    },
+  }
+
+}
---
+++ lua/custom/plugins/ui/lualine/themes/colors/default.lua
@@ -0,0 +1,47 @@
+local colors = {
+  black        = 'Black',
+  white        = 'NvimLightGray1',
+  red          = 'Red',
+  green        = 'NvimLightGreen',
+  blue         = 'NvimLightBlue',
+  yellow       = 'NvimLightMagenta',
+  gray         = 'DarkGrey',
+  darkgray     = 'NvimDarkGray2',
+  lightgray    = 'NvimDarkGray3',
+  inactivegray = 'NvimDarkGray1',
+}
+
+if vim.opt.background._value == "dark" then
+  return {
+    normal = {
+      a = { bg = colors.green, fg = colors.black, gui = 'bold' },
+      b = { bg = colors.darkgray, fg = colors.white },
+      c = { bg = colors.lightgray, fg = colors.gray }
+    },
+    insert = {
+      a = { bg = colors.blue, fg = colors.black, gui = 'bold' },
+      b = { bg = colors.lightgray, fg = colors.white },
+      c = { bg = colors.lightgray, fg = colors.white }
+    },
+    visual = {
+      a = { bg = colors.yellow, fg = colors.black, gui = 'bold' },
+      b = { bg = colors.darkgray, fg = colors.white },
+      c = { bg = colors.inactivegray, fg = colors.black }
+    },
+    replace = {
+      a = { bg = colors.red, fg = colors.black, gui = 'bold' },
+      b = { bg = colors.darkgray, fg = colors.white },
+      c = { bg = colors.black, fg = colors.white }
+    },
+    command = {
+      a = { bg = colors.yellow, fg = colors.black, gui = 'bold' },
+      b = { bg = colors.darkgray, fg = colors.white },
+      c = { bg = colors.inactivegray, fg = colors.black }
+    },
+    inactive = {
+      a = { bg = colors.inactivegray, fg = colors.gray, gui = 'bold' },
+      b = { bg = colors.inactivegray, fg = colors.gray },
+      c = { bg = colors.inactivegray, fg = colors.gray }
+    }
+  }
+end
