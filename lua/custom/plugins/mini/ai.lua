62afcea383d930f164fd03d08f0e79bdb014af0d
pvskp <pvincius14@gmail.com>
2024-07-20 19:45:59 -0300

    Fix mini.ai

--- lua/custom/plugins/mini/ai.lua
+++ lua/custom/plugins/mini/ai.lua
@@ -14,24 +14,27 @@ return {
     -- How to search for object (first inside current line, then inside
     -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
     -- 'cover_or_nearest', 'next', 'previous', 'nearest'.
-    search_method = 'cover',
-
+    search_method = 'cover_or_next',
   },
+
   config = function(_, opts)
     require('mini.ai').setup(opts)
 
-    local keymap_opts = { noremap = true, silent = true }
+    local keymap_opts = { noremap = false, silent = true }
     local tor = require("custom.plugins.mini.textobject.text_object_repeat")
 
-    local movements = { "in", "an", "il", "al" }
+    local movements = { "i", "a" }
     local characters = { "(", "[", "{", "<", ")", "]", "}", ">", "b", "\"", "'", "q", "?", "t", "f", "a" }
 
     for _, movement in ipairs(movements) do
       for _, char in ipairs(characters) do
         local keymap = movement .. char
-        vim.keymap.set('v', keymap, function() tor.execute_and_remember(keymap) end, keymap_opts)
+        vim.keymap.set('v', keymap, function()
+          tor.execute_and_remember(movement .. 'n' .. char)
+        end, keymap_opts)
       end
     end
+
 
     vim.keymap.set('v', 'n', tor.repeat_last_text_object, keymap_opts)
     vim.keymap.set('v', 'N', tor.inverse_repeat_last_text_object, keymap_opts)
