55f7fae0550da074a8cf99a6dcfedceb4249a8a7
pvskp <pvincius14@gmail.com>
2024-07-23 16:10:10 -0300

    feat: creates command to remove ANSI chars

--- lua/custom/commands.lua
+++ lua/custom/commands.lua
@@ -9,6 +9,10 @@ vim.api.nvim_create_user_command('Redir', function(ctx)
   vim.opt_local.modified = false
 end, { nargs = '+', complete = 'command' })
 
+vim.api.nvim_create_user_command('RemoveANSI', function(ctx)
+  vim.cmd([[ %s/\v(\x1b\[|\e\[)[0-9;]*[mGKH]//g ]])
+end, {})
+
 -- Copy text to clipboard using codeblock format ```{ft}{content}```
 vim.api.nvim_create_user_command('CopyCodeBlock', function(opts)
   local lines = vim.api.nvim_buf_get_lines(0, opts.line1 - 1, opts.line2, true)
