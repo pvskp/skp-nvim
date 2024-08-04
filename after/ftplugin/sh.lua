69e9e3ddf21565594c22e6894c182a4a17ceefa7
pvskp <pvincius14@gmail.com>
2024-08-04 12:06:30 -0300

    feat: better shell script execution

--- after/ftplugin/sh.lua
+++ after/ftplugin/sh.lua
@@ -6,9 +6,28 @@ end
 function _execute_shell_script() end
 
 vim.keymap.set('n', '<C-Space>', function()
-  local filename = vim.fn.expand '%'
+  local filename = vim.fn.expand('%')
+  local filedir = vim.fn.expand('%:p:h')
+
+  if vim.fn.filereadable(filename) == 0 then
+    vim.notify("File not found: " .. filename, vim.log.levels.ERROR)
+    return
+  end
+
+  if vim.fn.isdirectory(filedir) == 0 then
+    vim.notify("Directory not found: " .. filedir, vim.log.levels.ERROR)
+    return
+  end
+
   if not is_executable(filename) then
     vim.cmd('!chmod +x ' .. filename)
   end
-  vim.cmd('!./' .. filename)
-end, { noremap = true, silent = true })
+
+  local output = vim.fn.system({ './' .. filename })
+
+  if vim.v.shell_error ~= 0 then
+    vim.notify("Error executing script: " .. output, vim.log.levels.ERROR)
+  else
+    vim.notify(output)
+  end
+end, { noremap = true })
