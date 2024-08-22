6c41312731c864f086f9462556678a6a042479f2
pvskp <pvincius14@gmail.com>
2024-08-22 13:15:44 -0300

    checkpoint

--- lua/custom/plugins/colorscheme/rose-pine.lua
+++ lua/custom/plugins/colorscheme/rose-pine.lua
@@ -4,7 +4,7 @@ return {
   priority = 1000,
   lazy = false,
   opts = {
-    variant = 'main', -- auto, main, moon, or dawn
+    variant = 'main',      -- auto, main, moon, or dawn
     dark_variant = 'main', -- main, moon, or dawn
     dim_inactive_windows = false,
     extend_background_behind_borders = true,
@@ -12,17 +12,19 @@ return {
     enable = {
       terminal = true,
       legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-      migrations = true, -- Handle deprecated options automatically
+      migrations = true,        -- Handle deprecated options automatically
     },
 
     styles = {
       bold = true,
-      italic = true,
+      italic = false,
       transparency = false,
     },
   },
   config = function(_, opts)
     require('rose-pine').setup(opts)
     vim.cmd [[colorscheme rose-pine]]
+
+    set_hl("StatusLine", { fg = "#ffffff", bg = "#907aa9" })
   end,
 }
