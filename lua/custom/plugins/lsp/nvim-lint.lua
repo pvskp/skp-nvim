7533b56cc8c7ab473efd57b24b9cdaa2b295fd11
pvskp <pvincius14@gmail.com>
2024-08-29 09:40:30 -0300

    feat: add eslint_d as js/ts linter

--- lua/custom/plugins/lsp/nvim-lint.lua
+++ lua/custom/plugins/lsp/nvim-lint.lua
@@ -8,8 +8,8 @@ return {
     local lint = require 'lint'
 
     lint.linters_by_ft = {
-      -- javascript = { 'eslint_d' },
-      -- typescript = { 'eslint_d' },
+      javascript = { 'eslint_d' },
+      typescript = { 'eslint_d' },
       -- json = { 'jsonlint' },
       -- lua = { 'luacheck' },
       go = { 'golangcilint' },
