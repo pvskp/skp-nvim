044920d6a22c5adda5cd7d5885e86878e8244766
pvskp <pvincius14@gmail.com>
2024-08-31 02:54:08 -0300

    fix: remove command with warning

--- lua/custom/commands.lua
+++ lua/custom/commands.lua
@@ -2,13 +2,6 @@ vim.api.nvim_create_user_command('LspFormat', function()
   vim.lsp.buf.format()
 end, {})
 
-vim.api.nvim_create_user_command('Redir', function(ctx)
-  local lines = vim.split(vim.api.nvim_exec(ctx.args, true), '\n', { plain = true })
-  vim.cmd 'new'
-  vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
-  vim.opt_local.modified = false
-end, { nargs = '+', complete = 'command' })
-
 vim.api.nvim_create_user_command('RemoveANSI', function(ctx)
   vim.cmd [[ %s/\v(\x1b\[|\e\[)[0-9;]*[mGKH]//g ]]
 end, {})
