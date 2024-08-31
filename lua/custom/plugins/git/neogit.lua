8c9c2d84101f67e91ff7d8b7767135f4842dfe35
pvskp <pvincius14@gmail.com>
2024-08-31 02:58:30 -0300

    Locks neogit version

--- lua/custom/plugins/git/neogit.lua
+++ lua/custom/plugins/git/neogit.lua
@@ -1,11 +1,12 @@
 return {
   'NeogitOrg/neogit',
+  version = "1.0.0",
   cmd = 'Neogit',
   keys = {
     { '<leader>gn', '<cmd>Neogit<CR>', desc = 'Neogit' },
   },
   dependencies = {
-    'nvim-lua/plenary.nvim', -- required
+    'nvim-lua/plenary.nvim',  -- required
     'sindrets/diffview.nvim', -- optional - Diff integration
   },
   config = true,
