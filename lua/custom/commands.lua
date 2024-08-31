147a4cbddf763680565ce3d3ba30e68a0610eb9e
pvskp <pvincius14@gmail.com>
2024-08-31 13:55:58 -0300

    feat: create command to list snips

--- lua/custom/commands.lua
+++ lua/custom/commands.lua
@@ -6,6 +6,15 @@ vim.api.nvim_create_user_command('RemoveANSI', function(ctx)
   vim.cmd [[ %s/\v(\x1b\[|\e\[)[0-9;]*[mGKH]//g ]]
 end, {})
 
+vim.api.nvim_create_user_command("SnipList", function()
+  local ft_list = require("luasnip").available()[vim.o.filetype]
+  local ft_snips = {}
+  for _, item in pairs(ft_list) do
+    ft_snips[item.trigger] = item.name
+  end
+  print(vim.inspect(ft_snips))
+end, {})
+
 -- Copy text to clipboard using codeblock format ```{ft}{content}```
 vim.api.nvim_create_user_command('CopyCodeBlock', function(opts)
   local lines = vim.api.nvim_buf_get_lines(0, opts.line1 - 1, opts.line2, true)
