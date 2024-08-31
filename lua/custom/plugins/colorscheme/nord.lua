04101505329d1eac0f700c93fcb656f11aae6440
pvskp <pvincius14@gmail.com>
2024-08-31 02:58:54 -0300

    Fix nord color name

--- lua/custom/plugins/colorscheme/nord.lua
+++ lua/custom/plugins/colorscheme/nord.lua
@@ -3,43 +3,43 @@ local set_hl = function(hl_group_name, colors)
 end
 
 local function set_treesitter_hl()
-  set_hl('TreesitterContextLineNumber', { bg = nord.dark_gray })
-  set_hl('TreesitterContextBottom', { bg = nord.dark_gray, sp = nord.gray, underline = true }) -- Adds underline to TreesitterContextBottom
-  set_hl('TreesitterContext', { bg = nord.dark_gray })
-  set_hl('@string', { fg = nord.yellow })
+  set_hl('TreesitterContextLineNumber', { bg = Nord.dark_gray })
+  set_hl('TreesitterContextBottom', { bg = Nord.dark_gray, sp = Nord.gray, underline = true }) -- Adds underline to TreesitterContextBottom
+  set_hl('TreesitterContext', { bg = Nord.dark_gray })
+  set_hl('@string', { fg = Nord.yellow })
 
-  set_hl('@text.diff.add.diff', { bold = true, fg = nord.green })
-  set_hl('@text.diff.delete.diff', { bold = true, fg = nord.red })
+  set_hl('@text.diff.add.diff', { bold = true, fg = Nord.green })
+  set_hl('@text.diff.delete.diff', { bold = true, fg = Nord.red })
 end
 
 local function set_gitsigns_hl()
-  set_hl('GitSignsCurrentLineBlame', { fg = nord.light_gray_bright })
+  set_hl('GitSignsCurrentLineBlame', { fg = Nord.light_gray_bright })
 end
 
 local function set_telescope_hl()
   local TelescopePrompt = {
-    TelescopeNormal = { bg = nord.black },
+    TelescopeNormal = { bg = Nord.black },
     TelescopeBorder = { fg = 'NvimDarkGrey3', bg = 'NvimDarkGrey3' },
 
-    TelescopeResultsNormal = { bg = nord.dark_gray },
-    TelescopeResultsBorder = { fg = nord.dark_gray, bg = nord.dark_gray },
+    TelescopeResultsNormal = { bg = Nord.dark_gray },
+    TelescopeResultsBorder = { fg = Nord.dark_gray, bg = Nord.dark_gray },
     TelescopeResultTitle = {
-      fg = nord.dark_gray,
-      bg = nord.dark_gray,
+      fg = Nord.dark_gray,
+      bg = Nord.dark_gray,
     },
 
-    TelescopePreviewNormal = { bg = nord.gray },
-    TelescopePreviewBorder = { fg = nord.gray, bg = nord.gray },
-    TelescopePreviewTitle = { bg = nord.gray },
-    TelescopePreviewLine = { bg = nord.light_gray_bright },
+    TelescopePreviewNormal = { bg = Nord.gray },
+    TelescopePreviewBorder = { fg = Nord.gray, bg = Nord.gray },
+    TelescopePreviewTitle = { bg = Nord.gray },
+    TelescopePreviewLine = { bg = Nord.light_gray_bright },
 
-    TelescopePromptNormal = { fg = 'White', bg = nord.light_gray },
-    TelescopePromptBorder = { fg = nord.light_gray, bg = nord.light_gray },
-    TelescopePromptTitle = { fg = nord.light_gray, bg = nord.light_gray },
-    TelescopePromptCounter = { fg = nord.teal },
+    TelescopePromptNormal = { fg = 'White', bg = Nord.light_gray },
+    TelescopePromptBorder = { fg = Nord.light_gray, bg = Nord.light_gray },
+    TelescopePromptTitle = { fg = Nord.light_gray, bg = Nord.light_gray },
+    TelescopePromptCounter = { fg = Nord.teal },
 
-    TelescopeSelection = { fg = 'White', bg = nord.gray, bold = true },
-    TelescopeSelectionCaret = { fg = nord.green, bg = nord.gray },
+    TelescopeSelection = { fg = 'White', bg = Nord.gray, bold = true },
+    TelescopeSelectionCaret = { fg = Nord.green, bg = Nord.gray },
   }
 
   for hl, col in pairs(TelescopePrompt) do
@@ -48,8 +48,8 @@ local function set_telescope_hl()
 end
 
 local function other_color_improvements()
-  set_hl('MatchParen', { bg = nord.red })
-  set_hl('CursorlineNr', { fg = nord.orange, bg = nord.black, bold = true })
+  set_hl('MatchParen', { bg = Nord.red })
+  set_hl('CursorlineNr', { fg = Nord.orange, bg = Nord.black, bold = true })
 end
 
 -- set_telescope_hl()
