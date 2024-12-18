c10787c486d3d2c33e7439a8270f4d55f3e7df94
pvskp <pvincius14@gmail.com>
2024-12-18 11:57:11 -0300

    feat(blink.cmp): install blink.cmp but do not use it yet

---
+++ lua/custom/plugins/lsp/blink.lua
@@ -0,0 +1,43 @@
+return {
+  'saghen/blink.cmp',
+  lazy = false, -- lazy loading handled internally
+  -- optional: provides snippets for the snippet source
+  dependencies = {
+    {
+      'L3MON4D3/LuaSnip',
+      config = function()
+        local luasnip = require("luasnip")
+        luasnip.filetype_extend("javascriptreact", { "html" })
+        luasnip.filetype_extend("typescriptreact", { "html" })
+        require('luasnip.loaders.from_snipmate').lazy_load()
+        require('luasnip.loaders.from_vscode').lazy_load()
+      end
+    },
+    'rafamadriz/friendly-snippets',
+  },
+  version = 'v0.*',
+  opts = {
+    snippets = {
+      expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
+      active = function(filter)
+        if filter and filter.direction then
+          return require('luasnip').jumpable(filter.direction)
+        end
+        return require('luasnip').in_snippet()
+      end,
+      jump = function(direction) require('luasnip').jump(direction) end,
+    },
+    sources = {
+      default = { 'lsp', 'path', 'luasnip', 'buffer' },
+    },
+    windows = {
+      documentation = {
+        auto_show = true,
+      },
+    },
+    keymap = { preset = 'enter' },
+    completion = { accept = { auto_brackets = { enabled = true } } },
+    signature = { enabled = true },
+    opts_extend = { "sources.default" }
+  },
+}
