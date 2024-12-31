0f4dcbc6ad532b30e407506a52c45ec08efdd3e1
pvskp <pvincius14@gmail.com>
2024-12-31 10:03:17 -0300

    feat(fzf-lua): change fzf-lua current line colors

--- lua/custom/plugins/fzf-lua.lua
+++ lua/custom/plugins/fzf-lua.lua
@@ -13,7 +13,64 @@ return {
   config = function()
     -- calling `setup` is optional for customization
     local fzf_lua = require("fzf-lua")
-    fzf_lua.setup()
+    local actions = fzf_lua.actions
+    fzf_lua.setup({
+      fzf_colors = {
+        ["gutter"] = "-1",
+        ["pointer"] = "blue",
+        ["current-bg"] = "blue",
+      },
+      keymap = {
+        builtin = {
+          -- neovim `:tmap` mappings for the fzf win
+          -- true,        -- uncomment to inherit all the below in your custom config
+          ["<M-Esc>"]    = "hide",  -- hide fzf-lua, `:FzfLua resume` to continue
+          ["<F1>"]       = "toggle-help",
+          ["<F2>"]       = "toggle-fullscreen",
+          -- Only valid with the 'builtin' previewer
+          ["<F3>"]       = "toggle-preview-wrap",
+          ["<F4>"]       = "toggle-preview",
+          -- Rotate preview clockwise/counter-clockwise
+          ["<F5>"]       = "toggle-preview-ccw",
+          ["<F6>"]       = "toggle-preview-cw",
+          -- `ts-ctx` binds require `nvim-treesitter-context`
+          ["<F7>"]       = "toggle-preview-ts-ctx",
+          ["<F8>"]       = "preview-ts-ctx-dec",
+          ["<F9>"]       = "preview-ts-ctx-inc",
+          ["<S-Left>"]   = "preview-reset",
+          ["<C-f>"]      = "preview-page-down",
+          ["<C-b>"]      = "preview-page-up",
+          ["<C-j>"]      = "preview-page-down",
+          ["<C-k>"]      = "preview-page-up",
+          ["<M-S-down>"] = "preview-down",
+          ["<M-S-up>"]   = "preview-up",
+        },
+        fzf = {
+          -- fzf '--bind=' options
+          -- true,        -- uncomment to inherit all the below in your custom config
+          true, -- uncomment to inherit all the below in your custom config
+          ["ctrl-z"]     = "abort",
+          ["ctrl-u"]     = "unix-line-discard",
+          ["ctrl-f"]     = "",
+          ["ctrl-b"]     = "",
+          ["ctrl-a"]     = "beginning-of-line",
+          ["ctrl-e"]     = "end-of-line",
+          ["alt-a"]      = "toggle-all",
+          ["alt-g"]      = "first",
+          ["alt-G"]      = "last",
+          -- Only valid with fzf previewers (bat/cat/git/etc)
+          ["f3"]         = "toggle-preview-wrap",
+          ["f4"]         = "toggle-preview",
+          ["shift-down"] = "preview-page-down",
+          ["shift-up"]   = "preview-page-up",
+        },
+        actions = {
+          files = {
+            ["ctrl-q"] = actions.file_sel_to_qf,
+          }
+        }
+      },
+    })
     fzf_lua.register_ui_select()
 
     local set = vim.keymap.set
