84c2711df1292cecaa5b9d7d1cbd5004ea086aa9
pvskp <pvincius14@gmail.com>
2025-02-12 16:59:57 -0300

    removes blink dependencies

--- lua/custom/plugins/lsp/lspconfig.lua
+++ lua/custom/plugins/lsp/lspconfig.lua
@@ -4,7 +4,7 @@ return {
   event = { "BufReadPost", "BufWritePost", "BufNewFile" },
   dependencies = {
     -- 'hrsh7th/cmp-nvim-lsp', -- needed for lsp capabilities
-    "saghen/blink.cmp", -- needed for lsp capabilities
+    -- "saghen/blink.cmp", -- needed for lsp capabilities
     {
       'williamboman/mason.nvim',
       version = '*',
@@ -137,13 +137,14 @@ return {
 
 
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-    -- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
     --
-    capabilities = vim.tbl_deep_extend(
-      'force',
-      capabilities,
-      require('blink.cmp').get_lsp_capabilities(capabilities)
-    )
+
+    -- capabilities = vim.tbl_deep_extend(
+    --   'force',
+    --   capabilities,
+    --   require('blink.cmp').get_lsp_capabilities(capabilities)
+    -- )
 
     capabilities.textDocument.foldingRange = {
       dynamicRegistration = false,
