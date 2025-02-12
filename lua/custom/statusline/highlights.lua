f4373f0b979655114ea5eb4c18dec6182d941c0e
pvskp <pvincius14@gmail.com>
2025-02-12 16:59:48 -0300

    color my statusline and solve some bugs in it

--- lua/custom/statusline/highlights.lua
+++ lua/custom/statusline/highlights.lua
@@ -1,26 +1,26 @@
--- vim.api.nvim_set_hl(0, "IconMoon", {
---   fg = "yellow",
---   bg = vim.api.nvim_get_hl(0, { name = 'StatuslineMode' }).bg
--- })
---
--- vim.api.nvim_set_hl(0, "IconSun", {
---   fg = "yellow",
---   -- bg = vim.api.nvim_get_hl(0, { name = 'StatuslineMode' }).bg
--- })
---
--- vim.api.nvim_set_hl(0, "IconSunset", {
---   fg = "orange",
---   -- bg = vim.api.nvim_get_hl(0, { name = 'StatuslineMode' }).bg
--- })
---
--- vim.api.nvim_set_hl(0, "StatuslineMode", {
---   -- bg = "white",
---   -- fg = "black",
--- })
+vim.api.nvim_set_hl(0, "IconMoon", {
+  fg = "yellow",
+  bg = vim.api.nvim_get_hl(0, { name = 'StatuslineMode' }).bg
+})
 
--- vim.api.nvim_set_hl(0, "Statusline", {
---   -- bg = "black",
---   -- fg = "white",
--- })
+vim.api.nvim_set_hl(0, "IconSun", {
+  fg = "yellow",
+  -- bg = vim.api.nvim_get_hl(0, { name = 'StatuslineMode' }).bg
+})
+
+vim.api.nvim_set_hl(0, "IconSunset", {
+  fg = "orange",
+  -- bg = vim.api.nvim_get_hl(0, { name = 'StatuslineMode' }).bg
+})
+
+vim.api.nvim_set_hl(0, "StatuslineMode", {
+  -- bg = "white",
+  -- fg = "black",
+})
+
+vim.api.nvim_set_hl(0, "Statusline", {
+  -- bg = "black",
+  -- fg = "white",
+})
 
 
