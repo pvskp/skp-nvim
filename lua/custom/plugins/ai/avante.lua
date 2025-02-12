d1bb07d58a95f482fc9681c97b921b2b339c8a92
pvskp <pvincius14@gmail.com>
2025-02-12 16:59:24 -0300

    delete uneeded avante dependencies

--- lua/custom/plugins/ai/avante.lua
+++ lua/custom/plugins/ai/avante.lua
@@ -8,6 +8,7 @@ return {
     openai = {
       model = "gpt-4o-mini"
     },
+    hints = { enabled = false },
     windows = {
       ---@type "right" | "left" | "top" | "bottom"
       position = "right", -- the position of the sidebar
@@ -44,31 +45,5 @@ return {
     --- The below dependencies are optional,
     "hrsh7th/nvim-cmp",            -- autocompletion for avante commands and mentions
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua",      -- for providers='copilot'
-    -- {
-    --   -- support for image pasting
-    --   "HakonHarnes/img-clip.nvim",
-    --   event = "VeryLazy",
-    --   opts = {
-    --     -- recommended settings
-    --     default = {
-    --       embed_image_as_base64 = false,
-    --       prompt_for_file_name = false,
-    --       drag_and_drop = {
-    --         insert_mode = true,
-    --       },
-    --       -- required for Windows users
-    --       use_absolute_path = true,
-    --     },
-    --   },
-    -- },
-    -- {
-    --   -- Make sure to set this up properly if you have lazy=true
-    --   'MeanderingProgrammer/render-markdown.nvim',
-    --   opts = {
-    --     file_types = { "markdown", "Avante" },
-    --   },
-    --   ft = { "markdown", "Avante" },
-    -- },
   },
 }
