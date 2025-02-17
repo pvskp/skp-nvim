41225a6701abf017e4fe6239362cdbf825d723ea
pvskp <pvincius14@gmail.com>
2025-02-16 23:17:29 -0300

    checkpoint

--- lua/custom/plugins/lsp/barbecue.lua
+++ lua/custom/plugins/lsp/barbecue.lua
@@ -8,18 +8,21 @@ return {
   dependencies = {
     "SmiteshP/nvim-navic",
     "nvim-tree/nvim-web-devicons", -- optional dependency
-  },
-  opts = {
-    theme = {
-      normal = { bg = "#16161E" }
-    },
-    symbols = {
-      modified = "●",
-      ellipsis = "…",
-      separator = ">",
-    },
+    "nvim-treesitter/nvim-treesitter-context",
   },
   config = function(_, opts)
+    local tsctx = vim.api.nvim_get_hl(0, { name = "TreesitterContext" })
+
+    opts = {
+      theme = {
+        normal = { bg = tsctx.bg }
+      },
+      symbols = {
+        modified = "●",
+        ellipsis = "…",
+        separator = ">",
+      },
+    }
     require("barbecue").setup(opts)
   end
 }
