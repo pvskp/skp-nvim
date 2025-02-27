aebe56266f09777bd9290e44c3003b9f2416a5b4
pvskp <pvincius14@gmail.com>
2025-02-27 09:21:34 -0300

    Fix(MiniFiles): files not split

--- lua/custom/plugins/mini/files.lua
+++ lua/custom/plugins/mini/files.lua
@@ -65,7 +65,7 @@ return {
       local rhs = function()
         -- Make new window and set it as target
         local new_target_window
-        vim.api.nvim_win_call(MiniFiles.get_target_window(), function()
+        vim.api.nvim_win_call(MiniFiles.get_explorer_state().target_window, function()
           vim.cmd(direction .. ' split')
           new_target_window = vim.api.nvim_get_current_win()
         end)
