e272d8cb6a6987e57f15de3b435cdd04ab413f57
pvskp <pvincius14@gmail.com>
2025-03-09 12:37:35 -0300

    use basedpyright

--- lua/custom/plugins/colorscheme/vscode.lua
+++ lua/custom/plugins/colorscheme/vscode.lua
@@ -6,12 +6,13 @@ return {
     vim.o.background = 'dark'
     local c = require('vscode.colors').get_colors()
     local color_bg = c.vscDarkBlue
+    local darker_bg = "#1a1a1a"
     require('vscode').setup({
       -- Enable italic comment
       italic_comments = true,
 
       -- Underline `@markup.link.*` variants
-      underline_links = true,
+      underline_links = false,
 
       -- Disable nvim-tree background color
       disable_nvimtree_bg = true,
@@ -23,6 +24,8 @@ return {
       group_overrides = {
         -- this supports the same val table as vim.api.nvim_set_hl
         -- use colors from this colorscheme by requiring vscode.colors!
+        Pmenu = { bg = c.vscLeftMid },
+        NormalFloat = { bg = c.vscLeftDark },
         Cursor = { fg = color_bg, bg = c.vscLightGreen, bold = true },
         Statusline = { bg = c.vscLeftDark },
         StatuslineMode = { bg = color_bg },
@@ -31,9 +34,15 @@ return {
         IconSun = { fg = c.vscYellow, bg = color_bg },
         IconSunset = { fg = c.vscOrange, bg = color_bg },
         IconMoon = { fg = c.vscAccentBlue, bg = color_bg },
-        TreesitterContext = { bg = "#1a1a1a" },
-        TreesitterContextLineNumber = { bg = "#1a1a1a" },
-        TreesitterContextBottom = {underline = true, sp = "#000000"}
+        TreesitterContext = { bg = darker_bg },
+        TreesitterContextLineNumber = { bg = darker_bg },
+        TreesitterContextBottom = { underline = true, sp = "#000000" },
+        CursorLineNr = { fg = c.vscPink, bold = true },
+        MiniFilesFile = { bg = c.vscLeftDark },
+        MiniFilesDirectory = { bg = c.vscLeftDark, fg = c.vscAccentBlue },
+        MiniFilesNormal = { bg = c.vscLeftDark },
+        MiniFilesTitle = { bg = c.vscLeftDark },
+        MiniFilesTitleFocused = { bg = c.vscLeftDark, fg = c.vscAccentBlue},
       }
     })
 
