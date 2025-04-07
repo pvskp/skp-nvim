342633a4c4467b632c02b701db0210b162b0cf53
pvskp <pvincius14@gmail.com>
2025-04-07 19:41:30 -0300

    updates avante

--- lua/custom/plugins/ai/avante.lua
+++ lua/custom/plugins/ai/avante.lua
@@ -1,7 +1,7 @@
 return {
   "yetone/avante.nvim",
   event = "VeryLazy",
-  version = false, -- set this to "*" if you want to always pull the latest change, false to update on release
+  version = "v0.0.23", -- set this to "*" if you want to always pull the latest change, false to update on release
   opts = {
     provider = "openai",
     openai = {
@@ -11,12 +11,12 @@ return {
     windows = {
       ---@type "right" | "left" | "top" | "bottom"
       position = "right", -- the position of the sidebar
-      wrap = true,      -- similar to vim.o.wrap
-      width = 30,       -- default % based on available width
+      wrap = true,        -- similar to vim.o.wrap
+      width = 30,         -- default % based on available width
       sidebar_header = {
-        enabled = true, -- true, false to enable/disable the header
+        enabled = true,   -- true, false to enable/disable the header
         align = "center", -- left, center, right for title
-        rounded = true,
+        rounded = false,
       },
       input = {
         prefix = "> ",
@@ -27,14 +27,13 @@ return {
         start_insert = true, -- Start insert mode when opening the edit window
       },
       ask = {
-        floating = false,  -- Open the 'AvanteAsk' prompt in a floating window
-        start_insert = true, -- Start insert mode when opening the ask window
+        floating = false,     -- Open the 'AvanteAsk' prompt in a floating window
+        start_insert = false, -- Start insert mode when opening the ask window
         border = "rounded",
         ---@type "ours" | "theirs"
         focus_on_apply = "ours", -- which diff to focus after applying
       },
     },
-
   },
   build = "make",
   dependencies = {
