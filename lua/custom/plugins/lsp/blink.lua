6744faa19c8790389ee62c5387b3e9f2f589f6db
pvskp <pvincius14@gmail.com>
2025-04-16 22:56:26 -0300

    Uses blink cpm

--- lua/custom/plugins/lsp/blink.lua
+++ lua/custom/plugins/lsp/blink.lua
@@ -1,62 +1,119 @@
 return {
   'saghen/blink.cmp',
-  lazy = false, -- lazy loading handled internally
   -- optional: provides snippets for the snippet source
   dependencies = {
+    'rafamadriz/friendly-snippets',
     {
       'L3MON4D3/LuaSnip',
+      version = 'v2.*',
       config = function()
-        local luasnip = require("luasnip")
-        luasnip.filetype_extend("javascriptreact", { "html" })
-        luasnip.filetype_extend("typescriptreact", { "html" })
-        require('luasnip.loaders.from_vscode').lazy_load()
         require("luasnip.loaders.from_snipmate").lazy_load()
       end
     },
-    'rafamadriz/friendly-snippets',
+    "MahanRahmati/blink-nerdfont.nvim",
+    "moyiz/blink-emoji.nvim",
+    'xzbdmw/colorful-menu.nvim',
   },
-  version = 'v0.*',
+  lazy = false,
+  -- event = {
+  --   'InsertEnter',
+  --   -- 'CmdlineEnter'
+  -- },
+  version = '1.*',
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
   opts = {
-    snippets = {
-      expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
-      active = function(filter)
-        if filter and filter.direction then
-          return require('luasnip').jumpable(filter.direction)
-        end
-        return require('luasnip').in_snippet()
-      end,
-      jump = function(direction) require('luasnip').jump(direction) end,
-    },
-    sources = {
-      default = { 'lsp', 'path', 'luasnip', 'buffer' },
-      providers = {
-        lsp = {
-          async = true,
-          -- async = function()
-          --   local clients = vim.lsp.get_clients({ bufnr = 0 })
-          --   for _, client in ipairs(clients) do
-          --     if client.name == 'marksman' then
-          --       return true
-          --       local
-          --     end
-          --     return false
-          --   end
-          -- end
-        }
-      },
-    },
-    windows = {
-      documentation = {
-        auto_show = true,
-      },
-    },
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- 'super-tab' for mappings similar to vscode (tab to accept)
+    -- 'enter' for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
     keymap = {
       preset = 'enter',
-      ['<C-l>'] = {'snippet_forward', 'fallback'},
-      ['<C-h>'] = {'snippet_backward', 'fallback'},
+      ['<C-l>'] = { 'snippet_forward' },
+      ['<C-h>'] = { 'snippet_backward' },
+      -- ['<Tab>'] = { 'accept' }
     },
-    completion = { accept = { auto_brackets = { enabled = true } } },
+    appearance = {
+      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = 'mono'
+    },
+
+    cmdline = { enabled = true },
+    snippets = { preset = "luasnip" },
+    completion = {
+      menu = {
+        draw = {
+          columns = { { "kind_icon" }, { "label", gap = 1 } },
+          components = {
+            label = {
+              text = function(ctx)
+                return require("colorful-menu").blink_components_text(ctx)
+              end,
+              highlight = function(ctx)
+                return require("colorful-menu").blink_components_highlight(ctx)
+              end,
+            },
+          },
+          -- treesitter = {
+          --   "lsp",
+          -- }
+        }
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 0,
+      },
+      ghost_text = { enabled = true },
+    },
+
+    -- Default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = {
+      default = {
+        'lsp',
+        'path',
+        'snippets',
+        'buffer',
+        'nerdfont'
+      },
+      providers = {
+        nerdfont = {
+          module = "blink-nerdfont",
+          name = "Nerd Fonts",
+          score_offset = 15,        -- Tune by preference
+          opts = { insert = true }, -- Insert nerdfont icon (default) or complete its name
+        },
+        emoji = {
+          module = "blink-emoji",
+          name = "Emoji",
+          score_offset = 15,        -- Tune by preference
+          opts = { insert = true }, -- Insert emoji (default) or complete its name
+          should_show_items = function()
+            return vim.tbl_contains(
+            -- Enable emoji completion only for git commits and markdown.
+            -- By default, enabled for all file-types.
+              { "gitcommit", "markdown" },
+              vim.o.filetype
+            )
+          end,
+        }
+
+      }
+    },
+
+
     signature = { enabled = true },
-    opts_extend = { "sources.default" }
+    fuzzy = { implementation = "prefer_rust_with_warning" }
   },
+  opts_extend = { "sources.default" }
+
 }
