62cda2707c96e18fec39ef7a420a58bf4a61af65
pvskp <pvincius14@gmail.com>
2026-04-19 02:20:06 -0300

    feat(fzf-lua): different layout for ui_select

--- plugin/fzf-lua.lua
+++ plugin/fzf-lua.lua
@@ -2,7 +2,26 @@ vim.pack.add({
 	Gh("ibhagwan/fzf-lua"),
 })
 
+local fzf = require("fzf-lua")
+
 ---@module "fzf-lua"
 ---@type fzf-lua.Config|{}
 ---@diagnostic disable: missing-fields
-require("fzf-lua").setup({ "ivy" })
+fzf.setup({
+	"ivy",
+	ui_select = function(fzf_opts, items)
+		---@type fzf-lua.Config
+		local select_opts = {
+			winopts = {
+				fullscreen = false,
+        border = "single",
+        row = 0.5,
+        col = 0.5,
+        height = 20,
+        width = 100,
+			},
+		}
+
+		return vim.tbl_deep_extend("force", fzf_opts, select_opts)
+	end,
+})
